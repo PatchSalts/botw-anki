e4c2d6acefecb1e8789884ac059b28178c4eb3a6
Patrick Salts <patchsalts@gmail.com>
2020-10-23 23:15:53 -0400

    Added sorting.

--- generate-anki.sh
+++ generate-anki.sh
@@ -3,5 +3,5 @@
 [ -d "botw-master" ] || [ -f "master.zip" ] || curl -o master.zip https://codeload.github.com/leoetlino/botw/zip/master
 [ -d "botw-master" ] || unzip master.zip
 [ -f "master.zip" ] && rm master.zip
-grep -P -r -h -o -I -f japanese.pattern botw-master/Message/Msg_JPja.product.sarc > japanese.text
+grep -P -r -h -o -I -f japanese.pattern botw-master/Message/Msg_JPja.product.sarc | sort -u > japanese.text
 
