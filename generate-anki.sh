e89710a541712d3be0ff3b4270709d5fa9899f27
Patrick Salts <patchsalts@gmail.com>
2020-10-26 14:18:38 -0400

    I think it's done? Added furigana screening.

--- generate-anki.sh
+++ generate-anki.sh
@@ -4,5 +4,23 @@
 [ -d "botw-master" ] || unzip master.zip
 [ -f "master.zip" ] && rm master.zip
 grep -P -r -h -o -I -f japanese.pattern botw-master/Message/Msg_JPja.product.sarc | sort -u > japanese.text
-grep -P -x -f furistrip.pattern japanese.text > furigana.furistrip
-comm -3 japanese.text furigana.furistrip > japanese.furistrip
+grep -P -x -f leading-chars.pattern japanese.text > leading-chars.text
+comm -3 japanese.text leading-chars.text > non-leading-chars.text
+
+echo > leading-chars-removed.text
+for line in $(cat leading-chars.text); do
+	until [[ $prompt == "y" ]] || [[ $prompt == "n" ]]; do
+		echo ""
+		echo "$line"
+		read -n 1 -p "Is this furigana? [y/n]: " prompt
+	done
+	case $prompt in
+		"y")
+			echo "$line" | grep -P -o -f remove-leading.pattern >> leading-chars-removed.text
+			;;
+		"n")
+			echo "$line" >> leading-chars-removed.text
+			;;
+	esac
+	prompt=""
+done
